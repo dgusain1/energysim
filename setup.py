f1ecb20e1ede11cde093685f57d238c47cccab77
dgusain1 <d.gusain@tudelft.nl>
2020-06-29 09:58:19 +0200

    setup file

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
       name='energysim',
-      version='1.0.0',
+      version='1.0.1',
       description='Python package for cosimulation of multi-energy systems',
       long_description=readme(),
       long_description_content_type="text/markdown",
